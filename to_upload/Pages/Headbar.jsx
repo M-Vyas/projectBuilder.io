491d9bf739526743491b30fdbabd9ad7656ecd1b
M-Vyas <109711997+M-Vyas@users.noreply.github.com>
2024-05-09 03:37:41 -0700

    Add files via upload
    homepage, headbar and about

---
+++ to_upload/Pages/Headbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../assets/Style.css'
+
+function Headbar(){
+  return (
+    <nav>
+        <div>
+            <a href="">
+                LOGO
+            </a>
+        </div>
+      <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="">QUERY</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">USER PROFILE</a></li>
+      </ul> 
+    </nav>
+  )
+}
+
+export default Headbar
